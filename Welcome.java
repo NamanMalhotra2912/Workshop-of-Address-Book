0aca0fc7ad02b606721299791bca90cee705aa7d
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-03-21 18:21:37 +0530

    Welcome to Address Book Program

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+package AddressBook.AddressBookSystemUsingThread;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Address Book System");
+
+	}
+
+}
